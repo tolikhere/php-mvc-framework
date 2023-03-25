530028d098491e54559db6c053acba46c87f7fe9
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-03-25 20:38:38 +0300

    Implement input labels

--- models/User.php
+++ models/User.php
@@ -49,4 +49,15 @@ class User extends DbModel
     {
         return ['firstname', 'lastname', 'email', 'password', 'status'];
     }
+
+    public function labels(): array
+    {
+        return [
+            'firstname' => 'First name',
+            'lastname' => 'Last name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'confirmPassword' => 'Confirm password',
+        ];
+    }
 }
