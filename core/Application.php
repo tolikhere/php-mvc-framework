1ea8434ef5fd0a9ae56426cded0653cdc3145ab6
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-03-25 13:50:34 +0300

    Migrations and connection to database

--- core/Application.php
+++ core/Application.php
@@ -12,6 +12,7 @@ class Application
     public Router $router;
     public Request $request;
     public Response $response;
+    public Database $db;
     public static Application $app;
     public Controller $controller;
     public function __construct($rootPath)
@@ -21,6 +22,8 @@ class Application
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database();
     }
 
     public function run()
